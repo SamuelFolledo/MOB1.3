cee1b0acabbf82f38f251e829c5462e66631830c
Samuel Folledo <samuelfolledo@gmail.com>
2020-04-06 01:08:06 -0400

    [add] made cell and outlets

--- classwork/MOB1.3ClassWork/MOB1.3ClassWork/Project- GiphySearch/Views/GifCell.swift
+++ classwork/MOB1.3ClassWork/MOB1.3ClassWork/Project- GiphySearch/Views/GifCell.swift
@@ -9,16 +9,14 @@
 import UIKit
 
 class GifCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        // Add views
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
-
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 }
