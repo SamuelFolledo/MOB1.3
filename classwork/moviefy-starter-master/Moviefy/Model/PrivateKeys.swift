ff85a5456d8d88c4436fe2d6a96ee057c90b7c85
Samuel Folledo <samuelfolledo@gmail.com>
2020-04-29 18:17:04 -0400

    [add] moviefy generics

---
+++ classwork/moviefy-starter-master/Moviefy/Model/PrivateKeys.swift
@@ -0,0 +1,14 @@
+//
+//  PrivateKeys.swift
+//  Moviefy
+//
+//  Created by Macbook Pro 15 on 4/29/20.
+//  Copyright © 2020 Adriana González Martínez. All rights reserved.
+//
+
+import Foundation
+
+enum PrivateKey: String {
+    case movieKey = "467faceb327ee4f3be17cebdfabd0744"
+    case readAccessToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NjdmYWNlYjMyN2VlNGYzYmUxN2NlYmRmYWJkMDc0NCIsInN1YiI6IjVlYTlmYjAyYTEyODU2MDAxYjY4MDkxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pC37VfCRJ4G3_qVBmMjdg_ZOeuBfrFN_5RkjW89yZxw"
+}
